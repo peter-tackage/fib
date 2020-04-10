bd4d72d808ef38e3131689e50ce9d57ee6b63faa
peter <peter.tackage@gmail.com>
2020-04-10 23:52:25 +0200

    Use ViewBinding

--- app/src/main/java/com/petertackage/fibonacci/MainActivity.kt
+++ app/src/main/java/com/petertackage/fibonacci/MainActivity.kt
@@ -1,26 +1,26 @@
 package com.petertackage.fibonacci
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.petertackage.fibonacci.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var fibonacciTextView: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        fibonacciTextView = findViewById(R.id.textview_main_fibonacci)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         val model: MainActivityViewModel by viewModels()
 
         model.getSequence()
             .observe(this,
                 Observer { values ->
-                    fibonacciTextView.text =
+                    binding.textviewMainFibonacci.text =
                         values.joinToString(separator = "\n")
                 })
     }
