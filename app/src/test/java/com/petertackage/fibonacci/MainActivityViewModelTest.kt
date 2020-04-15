88b75472efd15ef95a3bb31293a98ddd9b17ba21
peter <peter.tackage@gmail.com>
2020-04-15 19:19:20 +0200

    Add test for result streaming

--- app/src/test/java/com/petertackage/fibonacci/MainActivityViewModelTest.kt
+++ app/src/test/java/com/petertackage/fibonacci/MainActivityViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.petertackage.fibonacci
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.petertackage.livedatatest.test
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.assertj.core.api.Assertions.assertThat
@@ -10,6 +11,9 @@ import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class MainActivityViewModelTest {
+    companion object {
+        const val TESTING_DELAY: Long = 50
+    }
 
     @get:Rule
     val liveDataMainThreadRule = InstantTaskExecutorRule()
@@ -21,8 +25,39 @@ class MainActivityViewModelTest {
 
     @Before
     fun setUp() {
-        viewModel = MainActivityViewModel(defaultDispatcher = coroutinesTestRule.testDispatcher)
+        viewModel = MainActivityViewModel(
+            defaultDispatcher = coroutinesTestRule.testDispatcher,
+            testingDelayMillis = TESTING_DELAY
+        )
     }
+
+    @Test
+    fun `fibonacci streams sequence`() =
+        coroutinesTestRule.testDispatcher.runBlockingTest {
+            val observer = viewModel.sequence.test()
+            assertThat(observer.values).containsExactly(listOf(0)) // sanity check
+
+            advanceTimeBy(3 * TESTING_DELAY)
+
+            assertThat(observer.values)
+                .containsExactly(
+                    listOf(0),
+                    listOf(0, 1),
+                    listOf(0, 1, 1),
+                    listOf(0, 1, 1, 2)
+                )
+
+            advanceTimeBy(TESTING_DELAY)
+
+            assertThat(observer.values)
+                .containsExactly(
+                    listOf(0),
+                    listOf(0, 1),
+                    listOf(0, 1, 1),
+                    listOf(0, 1, 1, 2),
+                    listOf(0, 1, 1, 2, 3)
+                )
+        }
 
     @Test
     fun `fibonacci sequence calculates all elements`() =
