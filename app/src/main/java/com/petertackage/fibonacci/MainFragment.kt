fd4e0e4c8167d262361b93f2a1f95bffe4f2549a
peter <peter.tackage@gmail.com>
2020-05-25 15:54:58 +0200

    Use Navigation component

--- app/src/main/java/com/petertackage/fibonacci/MainFragment.kt
+++ app/src/main/java/com/petertackage/fibonacci/MainFragment.kt
@@ -1,31 +1,46 @@
 package com.petertackage.fibonacci
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.petertackage.fibonacci.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
-    private lateinit var viewModel: MainViewModel
+    // Using technique from https://developer.android.com/topic/libraries/view-binding
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonMainFibonacci.setOnClickListener { findNavController().navigate(R.id.action_mainFragment_to_fibonacciFragment) }
+        binding.buttonMainMeme.setOnClickListener { findNavController().navigate(R.id.action_mainFragment_to_memeFragment) }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        val viewModel: MainViewModel by viewModels()
+
+        // TODO Use ViewModel
     }
 
 }
